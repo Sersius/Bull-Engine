101f244c931635bd7c5866f8170c61dc87050024
VictorTirado <arlaquim@gmail.com>
2019-07-07 14:00:23 +0200

    ImGui folder added (not working in code)

--- Bull-Engine/ModuleSceneIntro.cpp
+++ Bull-Engine/ModuleSceneIntro.cpp
@@ -4,6 +4,7 @@
 #include "Primitive.h"
 #include "PhysBody3D.h"
 
+
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
 }
@@ -37,6 +38,8 @@ update_status ModuleSceneIntro::Update(float dt)
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
+
 
 	return UPDATE_CONTINUE;
 }

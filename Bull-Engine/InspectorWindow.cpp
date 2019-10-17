b492aead95eb27403e65e77f6e13a62ef4c2fe2b
Sersius <segoro97@msn.com>
2019-10-17 18:18:32 +0200

    Inspector window enabled (WIP)

--- Bull-Engine/InspectorWindow.cpp
+++ Bull-Engine/InspectorWindow.cpp
@@ -24,8 +24,53 @@ bool InspectorWindow::Start()
 
 void InspectorWindow::Draw()
 {
-	if (ImGui::Begin("Hierarchy", &on, 0)) {
+	ImGuiWindowFlags flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+	if (ImGui::Begin("Inspector", &on, flags)) {
 
+		ImGui::SetWindowPos(ImVec2((App->width / 4) * 3, 20), ImGuiCond_Always);
+		ImGui::SetWindowSize(ImVec2(App->width / 4, 795), ImGuiCond_Always);
+		SDL_GetWindowSize(App->window->window, &App->width, &App->height);
+
+		ImGui::Text("Model Name: %s");
+		ImGui::Text("Model Path: %s");
+
+		if (ImGui::CollapsingHeader("Transform")) {
+			ImGui::Separator();
+
+			ImGui::Text("Position:");
+			ImGui::Text("[%f]    [%f]    [%f]");
+
+			ImGui::Text("Rotation:");
+			ImGui::Text("[%f]    [%f]    [%f]");
+
+			ImGui::Text("Scale:");
+			ImGui::Text("[%f]    [%f]    [%f]");
+		}
+
+		if (ImGui::CollapsingHeader("Mesh Information")) {
+			ImGui::Separator();
+
+			ImGui::Text("Mesh triangles: %i");
+
+			ImGui::Text("Mesh vertices: %i");
+
+			ImGui::Text("Mesh indices: %i");
+
+			ImGui::Text("Mesh normals: %f");
+
+			ImGui::Text("Mesh uvs: %f");
+		}
+
+		if (ImGui::CollapsingHeader("Material")) {
+			ImGui::Separator();
+
+			ImGui::Text("Texture path: %s");
+
+			ImGui::Text("Texture WIDTH: %i");
+
+			ImGui::Text("Texture HEIGHT: %i");
+		}
+	
 	}
 
 	ImGui::End();

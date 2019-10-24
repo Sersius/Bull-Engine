e82a5a99786b4b7428021b4e415ff5ebaec44586
Sersius <segoro97@msn.com>
2019-10-24 10:17:03 +0200

    Added plane, sphere and cone primitives

--- Bull-Engine/AddWindow.cpp
+++ Bull-Engine/AddWindow.cpp
@@ -26,7 +26,7 @@ void AddWindow::Draw()
 	ImGui::Begin("Enter the parameters", &on, 0);
 
 		static int current_item = 0;
-		ImGui::Combo("Select the object", &current_item, "Plane\0Pyramid\0Cube");
+		ImGui::Combo("Select the object", &current_item, "Plane\0Cube\0Sphere\0Cone");
 		
 		ImGui::Text("Scale");
 		ImGui::InputInt("", &scale);
@@ -42,11 +42,38 @@ void AddWindow::Draw()
 		ImGui::ColorEdit3("", color);
 		ImGui::Separator();
 
-		if (current_item == 2 && ImGui::Button("Create"))
+		if (current_item == 0 && ImGui::Button("Create"))
+		{
+			par_shapes_mesh* plane = par_shapes_create_plane(3,3);
+			InfoFbx mesh = App->loadFBX->LoadParShapeMesh(plane);
+			par_shapes_free_mesh(plane);
+
+			GameObject* new_go = new GameObject();
+		}
+
+		if (current_item == 1 && ImGui::Button("Create"))
 		{
 			par_shapes_mesh* cube = par_shapes_create_cube();
 			InfoFbx mesh = App->loadFBX->LoadParShapeMesh(cube);
 			par_shapes_free_mesh(cube);
+
+			GameObject* new_go = new GameObject();
+		}
+
+		if (current_item == 2 && ImGui::Button("Create"))
+		{
+			par_shapes_mesh* sphere = par_shapes_create_subdivided_sphere(2);
+			InfoFbx mesh = App->loadFBX->LoadParShapeMesh(sphere);
+			par_shapes_free_mesh(sphere);
+
+			GameObject* new_go = new GameObject();
+		}
+
+		if (current_item == 3 && ImGui::Button("Create"))
+		{
+			par_shapes_mesh* cone = par_shapes_create_cone(10,10);
+			InfoFbx mesh = App->loadFBX->LoadParShapeMesh(cone);
+			par_shapes_free_mesh(cone);
 
 			GameObject* new_go = new GameObject();
 		}

6275eb3534510d5881da7ba1382ced829ae660f9
Sersius <segoro97@msn.com>
2019-11-24 22:13:49 +0100

    Clean code

--- Bull-Engine/AddWindow.cpp
+++ Bull-Engine/AddWindow.cpp
@@ -31,20 +31,6 @@ void AddWindow::Draw()
 		static int current_item = 0;
 		ImGui::Combo("Select the object", &current_item, "Plane\0Cube\0Sphere\0Cone");
 		
-		ImGui::Text("Scale");
-		ImGui::InputInt("", &scale);
-		if (scale <= 0)
-			scale = 1;
-		ImGui::Separator();
-		
-		ImGui::Text("Position");
-		ImGui::InputInt3("", pos);
-		ImGui::Separator();
-		
-		ImGui::Text("Color");
-		ImGui::ColorEdit3("", color);
-		ImGui::Separator();
-
 		if (current_item == 0 && ImGui::Button("Create"))
 		{
 			par_shapes_mesh* plane = par_shapes_create_plane(3,3);
